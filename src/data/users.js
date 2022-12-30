18ae13d08dc3625a42d56c1eee1cdb74afbede84
MarcinPlaza1 <plazamarcin098@gmail.com>
2022-12-31 00:03:49 +0100

    Git initial files

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    name: 'Raegan Corine',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Roberto Cecilie',
+    attendance: '23%',
+    average: '3.3',
+  },
+  {
+    name: 'Kazimierz Galo',
+    attendance: '45%',
+    average: '4.3',
+  },
+  {
+    name: 'Olga Hahn',
+    attendance: '56%',
+    average: '4.1',
+  },
+  {
+    name: 'Charlotte Kimberlee',
+    attendance: '29%',
+    average: '2.4',
+  },
+  {
+    name: 'Arthur Morgan',
+    average: '2.4',
+  },
+];
